88d8a4c9c1d27a7b45a9fe2db4da5c95ff19de1e
MaximilianWebb <max.t.webb@gmail.com>
2023-11-30 11:44:24 -0700

    trash free

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// import {$, jQuery} from 'jQuery'
-$(function() { 
-    Cookies.set('cookie_name', 'cookie_value', { expires: 365 });
-    Cookies.get('cookie_name'); // => 'value'
-    Cookies.remove('cookie_name');
-});
-
-
 function loginInfo(){
     var user = document.getElementById("loginuser").value; 
     var pass = document.getElementById("loginpass").value;
