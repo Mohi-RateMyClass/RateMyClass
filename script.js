0c001d81ee3cfc773fe2bb9f729db9495ecc1294
MaximilianWebb <max.t.webb@gmail.com>
2023-12-01 20:53:13 -0700

    pain suffering & a true desire to punch my screen

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ function loginInfo(){
     var pass = document.getElementById("loginpass").value;
 alert(pass);
 }
+
+
+
+function tableMaker(id){
+     fetch(id).then(res => res.text()).then(csv => {
+
+            table.innerHTML = "";
+
+            let rows = csv.split("\n");
+            for(let row of rows){
+
+            let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
+
+            if (cols != null){
+                let tr = table.insertRow();
+                for(let col of cols){
+                    let td = tr.insertCell();
+                    td.innerHTML = col;
+                }
+            }
+        }
+    });    
+    };
+    
